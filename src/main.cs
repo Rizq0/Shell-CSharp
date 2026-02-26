60c40a1ab5b079878db2aee5cb0213127d8a8148
Rizq0 <joe_ethan2@hotmail.com>
2026-02-26 00:12:10 +0000

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -3,6 +3,7 @@ class Program
     static void Main()
     {
         bool isRunning = true;
+        List<string> commands = new List<string> { "echo", "exit", "type" };
         while (isRunning)
         {
             Console.Write("$ ");
@@ -14,6 +15,19 @@ class Program
             else if (input.ToLower().StartsWith("echo "))
             {
                 Console.WriteLine(input.Substring(5));
+            }
+            else if (input.ToLower().StartsWith("type "))
+            {
+                string currentCommand = input.Substring(5).Trim();
+                bool isBuiltin = commands.Any(c => c.Contains(currentCommand.ToLower()));
+                if (isBuiltin)
+                {
+                    Console.WriteLine($"{currentCommand} is a shell builtin");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentCommand}: not found");
+                }
             }
             else
             {
